a9a43ceb672ead99373bfe121b729db17a66757f
Arthur Rio <arthur@punchtab.com>
2013-05-10 16:22:47 -0700

    add some constants

--- exacttarget/constants.py
+++ exacttarget/constants.py
@@ -1,2 +1,6 @@
 OAUTH_HEADER_URL = "http://exacttarget.com"
 REST_API_BASE_URL = "https://auth.exacttargetapis.com/v1/"
+VALID_RESULT_STATUS = {
+    "MORE_DATA_AVAILABLE": "MoreDataAvailable",
+    "OK": "OK"
+}
